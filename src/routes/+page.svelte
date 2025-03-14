054b7b9ac8e7829948b60cfc061d95fe9127acff
Iyyafi Qolbi <iyyafi@gmail.com>
2025-03-15 01:00:28 +0700

    update style with daisy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,22 +17,36 @@
 	};
 </script>
 
-<input type="text" bind:value />
-<button onclick={handleGenerate}>Generate</button>
-<button onclick={handleReset}>Reset</button>
+<div class="mx-auto flex max-w-[1000px] flex-col gap-6 py-24">
+	<div class="flex flex-col gap-6">
+		<input type="text" bind:value class="input w-full" placeholder="Link here" />
+		<div class="grid grid-cols-3 gap-6">
+			<button onclick={handleGenerate} class="btn btn-primary"> Generate </button>
+			<button onclick={handleReset} class="btn btn-warning">Reset</button>
 
-{#if fixValue}
-	<QRCode
-		data={fixValue}
-		downloadUrlFileFormat="png"
-		dispatchDownloadUrl
-		on:downloadUrlGenerated={(event) => handleDownloadUrlGenerated(event.detail.url)}
-		logoPath={'/sesabyldc.png'}
-		logoSize={10}
-		size={1000}
-	/>
-{/if}
-
-{#if downloadUrl}
-	<a href={downloadUrl} download="QR-code-filename.png" target="_blank">Download QR Code</a>
-{/if}
+			{#if downloadUrl}
+				<a
+					href={downloadUrl}
+					download={`${Date.now()}.png`}
+					target="_blank"
+					class="btn btn-secondary"
+				>
+					Download QR Code
+				</a>
+			{/if}
+		</div>
+	</div>
+	<div>
+		{#if fixValue}
+			<QRCode
+				data={fixValue}
+				downloadUrlFileFormat="png"
+				dispatchDownloadUrl
+				on:downloadUrlGenerated={(event) => handleDownloadUrlGenerated(event.detail.url)}
+				logoPath={'/sesabyldc.png'}
+				logoSize={10}
+				size={1000}
+			/>
+		{/if}
+	</div>
+</div>
